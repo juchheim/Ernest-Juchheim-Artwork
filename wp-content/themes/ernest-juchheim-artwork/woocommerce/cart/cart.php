4b04d57e7edb6452af5a2635cc4627c1e861b13c
Ernest Juchheim <64336436+juchheim@users.noreply.github.com>
2024-07-23 13:50:09 -0500

    Update cart.php

--- wp-content/themes/ernest-juchheim-artwork/woocommerce/cart/cart.php
+++ wp-content/themes/ernest-juchheim-artwork/woocommerce/cart/cart.php
@@ -19,6 +19,8 @@ defined( 'ABSPATH' ) || exit;
 
 do_action( 'woocommerce_before_cart' ); ?>
 
+<div>HEY!</div>
+
 <form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
 	<?php do_action( 'woocommerce_before_cart_table' ); ?>
 
